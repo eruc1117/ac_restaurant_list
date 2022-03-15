a0f73903675fd4fad394944459641f380596c5b4
eruc <eruc101010@gmail.com>
2022-03-15 13:59:38 +0800

    feat: add restaurant seeder and Model

---
+++ function/customizeFun.js
@@ -0,0 +1,46 @@
+function predataEdit(array) {
+  let list = array.results
+  for (let index = 0; index < list.length; index++) {
+    //先對資料進行處理，增加sameName作為key，名稱和英文名稱(name and name_en)完全相同value為false，不同為true
+    list[index].name === list[index].name_en ?
+      list[index].sameName = false :
+      list[index].sameName = true
+  }
+  return list
+}
+function phoneSet(str) {
+  //補充對電話號碼格式的修改，比較符合習慣
+  return str.replace(/[\s]/g, '-')
+}
+
+
+function bodyDataEdit(listBody) {
+  listBody.name = listBody.name.trim()
+  listBody.name_en = listBody.name_en.trim()
+  listBody.category = listBody.category.trim()
+  listBody.location = listBody.location.trim()
+  listBody.description = listBody.description.trim()
+  listBody.sameName = listBody.name !== listBody.name_en
+  return listBody
+}
+
+function createNew(listBody) {
+  listBody.id = Number(listBody.id.trim())
+  listBody.name = listBody.name.trim()
+  listBody.name_en = listBody.name_en.trim()
+  listBody.category = listBody.category.trim()
+  listBody.location = listBody.location.trim()
+  listBody.description = listBody.description.trim()
+  listBody.sameName = listBody.name !== listBody.name_en
+  return listBody
+}
+
+
+
+//將模組輸出
+module.exports = {
+  predataEdit,
+  bodyDataEdit,
+  createNew,
+  phoneSet
+}
---
+++ models/restaurantModel.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const cusFunction = require('../function/customizeFun')
+const restaurantSchema = new Schema({
+  id: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  name_en: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  category: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: [true, 'Where is the restaurant?'],
+    trim: true
+  },
+  phone: {
+    type: String,
+    set: cusFunction.phoneSet,
+    validate: {
+      validator: function (phoneNumber) {
+        return /\d{2}-\d{4}-\d{4}/.test(phoneNumber);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: true
+  },
+  google_map: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  rating: {
+    type: Number,
+    man: 0,
+    max: 5,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  sameName: {
+    type: Boolean,
+    required: false
+  }
+})
+
+module.exports = mongoose.model('RestaurantModel', restaurantSchema)
