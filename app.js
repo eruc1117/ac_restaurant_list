d9691354ead30bd6b327ae281d5556aba48d898d
eruc <eruc101010@gmail.com>
2022-03-15 10:40:12 +0800

    feat: add static index page and add css by scss

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 const exphbs = require('express-handlebars')//require template engine
+const path = require('path')
+const sassMiddleware = require('node-sass-middleware')//require scss
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(
+  sassMiddleware({
+    src: path.join(__dirname, 'scss'),
+    dest: path.join(__dirname, 'public'),
+    debug: true,
+    outputStyle: 'compressed',
+  })
+)
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index')
