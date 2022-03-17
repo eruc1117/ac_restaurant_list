ae3defaad44b94953a5dac699de9cf9d8a6c8a37
eruc <eruc101010@gmail.com>
2022-03-17 21:02:53 +0800

    feat: add create new restaurant function

--- routes/modules/crud.js
+++ routes/modules/crud.js
@@ -3,12 +3,12 @@ const router = express.Router()
 const RestaurantModel = require('../../models/restaurantModel')
 const cusFunction = require('../../function/customizeFun')
 
-router.get('/:id', (req, res) => {
+router.get('/read/:id', (req, res) => {
   const id = req.params.id
   async function createShowPage(id) {
     try {
       const restaurantInfo = await RestaurantModel.findOne({ id }).lean()
-      res.render('show', {cssStyle: '/stylesheets/show.css', restaurantInfo})
+      res.render('show', { cssStyle: '/stylesheets/show.css', restaurantInfo })
     } catch (err) {
       console.log(err)
     }
@@ -22,13 +22,13 @@ router.get('/edit/:id', (req, res) => {
   async function createEditPage(id) {
     try {
       const restaurant = await RestaurantModel.findOne({ id }).lean()
-      res.render('edit', {cssStyle: '/stylesheets/edit.css', restaurant})
+      res.render('edit', { cssStyle: '/stylesheets/edit.css', restaurant })
     } catch (err) {
       console.log(err)
     }
   }
   createEditPage(id)
-}) 
+})
 
 router.post('/edit/:id', (req, res) => {
   const id = req.params.id
@@ -38,6 +38,30 @@ router.post('/edit/:id', (req, res) => {
   return RestaurantModel.findOneAndUpdate({ id }, newBody)
     .then(() => res.redirect(`/crud/${id}`))
     .catch(error => console.log(error))
+})
+
+//新增餐廳相關路由
+router.get('/create', (req, res) => {
+  res.render('new', {
+    cssStyle: '/stylesheets/edit.css'
+  })
+})
+
+router.post('/create', (req, res) => {
+  async function createRestaurant() {
+    try {
+      const newRestaurant = req.body
+      const totalInfo = await RestaurantModel.find()
+      totalInfo.length !== 0 ?
+        newRestaurant['id'] = Number(totalInfo[totalInfo.length - 1].id) + 1 :
+        newRestaurant['id'] = 1
+      await RestaurantModel.create(newRestaurant)
+      res.redirect('/')
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  createRestaurant()
 })
 
 module.exports = router
