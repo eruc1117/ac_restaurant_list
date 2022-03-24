1f3162807e65bd9239aa99823a251eb022fb3f9d
eruc <eruc101010@gmail.com>
2022-03-24 20:17:11 +0800

    feat: add user authentication

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 // 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
+const { authenticator } = require('../middleware/auth')
 
 //載入路由
 const restaurantList = require('./modules/restaurantList')
@@ -9,16 +10,16 @@ const sort = require('./modules/sort')
 const search = require('./modules/search')
 const user = require('./modules/user')
 
-// 將網址結構符合 / 字串的 request 導向 restaurantList 模組 
-router.use('/', restaurantList)
 // 將網址結構符合 /restaurants 字串的 request 導向 crud 模組 
-router.use('/restaurants', restaurants)
+router.use('/restaurants', authenticator, restaurants)
 // 將網址結構符合 /sort 字串的 request 導向 sort 模組 
-router.use('/sort', sort)
+router.use('/sort', authenticator, sort)
 // 將網址結構符合 /search 字串的 request 導向 search 模組 
-router.use('/search', search)
+router.use('/search', authenticator, search)
 // 將網址結構符合 /user 字串的 request 導向 user 模組 
 router.use('/user', user)
+// 將網址結構符合 / 字串的 request 導向 restaurantList 模組 
+router.use('/', authenticator, restaurantList)
 
 
 // 匯出路由器
